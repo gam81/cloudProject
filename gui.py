8d63d7585d182452c4bde711e16ef02cebdaa5d1
Giovanni Meiser <70547206+gam81@users.noreply.github.com>
2021-11-28 23:41:04 -0500

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,35 +1,41 @@
 import tkinter as gui
-
-def main():
-    window = gui.Tk()
-    window.title('Cloud Project')
-    window.geometry('720x360')
-
-    hadoop_button = gui.Button(window, text='Apache Hadoop', command=hd)
-    hadoop_button.pack(side='top')
-
-    spark_button = gui.Button(window, text='Apache Spark', command=sp)
-    spark_button.pack(side='left')
-
-    jupyter_button = gui.Button(window, text='Jupyter Notebook', command=jp)
-    jupyter_button.pack(side='bottom')
-
-    sonar_button = gui.Button(window, text='SonarQube and SonarScanner', command=sn)
-    sonar_button.pack(side='right')
-
-    window.mainloop()
+import os
 
 def hd():
-    pass
+    link = 'localhost:9870' if local else '35.223.112.91:9870'
 
 def sp():
-    pass
+    link = 'localhost:8080' if local else '34.132.29.196:8080'
 
 def jp():
-    pass
+    link = 'localhost:8888' if local else '34.136.84.126:8888'
 
 def sn():
-    pass
+    link = 'localhost:9000' if local else '104.197.67.245:9000'
+    
+def main():
+    global local 
+    
+    if len(sys.argv) > 1:
+        local = sys.argv[1].lower() == 'true'
+        
+    view = gui.Tk()
+    view.title('Cloud Project')
+    view.geometry('720x360')
+    
+    hadoop_button = gui.Button(view, text='Apache Hadoop', command=hd)
+    hadoop_button.pack(side='top')
+    
+    sonar_button = gui.Button(view, text='SonarQube and SonarScanner', command=sn)
+    sonar_button.pack(side='right')
+    
+    spark_button = gui.Button(view, text='Apache Spark', command=sp)
+    spark_button.pack(side='left')
+
+    jupyter_button = gui.Button(view, text='Jupyter Notebook', command=jp)
+    jupyter_button.pack(side='bottom')
+
+    view.mainloop()
 
 if __name__ == '__main__':
     main()
